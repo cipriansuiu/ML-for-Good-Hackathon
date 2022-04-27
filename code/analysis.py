70488ad5a4e0a2b58367dffa8278bc7738098f21
cipriansuiu <shuiu96@gmail.com>
2022-04-27 18:58:20 +0000

    licenta roxana

---
+++ code/analysis.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import os
+for root,dirs,files in os.walk(r'C:\Users\Suiu\PycharmProjects\ML-for-Good-Hackathon\code\data', topdown=False):
+    for name in files:
+        file_name = os.path.join(root, name)
+        writer = pd.ExcelWriter(os.path.join('output/',name))
+        df = pd.read_excel(file_name)
+        df.dropna()
+        df_mean = df.mean()
+        pd.options.display.float_format = '{:,.2f}'.format
+        df_median = df.median()
+        df_min = df.min()
+        df_max = df.max()
+        df_max.describe()
+        df_mean.to_excel(writer,'mean')
+        df_median.to_excel(writer,'median')
+        pd.DataFrame(df_min).to_excel(writer,'min')
+        pd.DataFrame(df_max).to_excel(writer, 'max')
+        writer.save()
+
+
+
+
+
+
